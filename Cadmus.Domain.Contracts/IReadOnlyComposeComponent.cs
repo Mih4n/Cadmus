6196815cd3c5e50164f4b7f96180c29f20d99f6a
mih4n <losevmisha0@gmail.com>
2025-11-07 15:17:11 +0300

    fix

--- Cadmus.Domain.Contracts/IReadOnlyComposeComponent.cs
+++ Cadmus.Domain.Contracts/IReadOnlyComposeComponent.cs
@@ -5,6 +5,8 @@ namespace Cadmus.Domain.Contracts;
 public interface IReadOnlyComposeComponent
 {
     T? GetComponent<T>() where T : IComponent;
+    int GetComponentCount<T>() where T : IComponent;
     bool TryGetComponent<T>([MaybeNullWhen(false)] out T component) where T : IComponent;
-    bool HasComponent<T>() where T : IComponent; 
+    bool HasComponent<T>() where T : IComponent;
+    IEnumerable<T> GetComponents<T>() where T : IComponent;
 }
